fb3fae0f8c874a0884835cd19057eb1b32a0ac2b
Francis Lau <flau1019@gmail.com>
2024-07-11 11:31:27 -0700

    First try solution to ReverseSubstring leetcode

---
+++ ReverseSubstringsBetweenEachPair.py
@@ -0,0 +1,12 @@
+def reverseParentheses(s):
+    while s.count(")") > 0:
+        position = 0
+        for i in range(s.index(")")): 
+            if s[s.index(")")-i-1] == "(":
+                position = s.index(")")-i-1
+                break
+        s = s[0:position] + s[s.index(")")-1:position:-1] + s[s.index(")")+1:]
+    return s
+
+s = "(u(love)i)"
+print(reverseParentheses(s))
